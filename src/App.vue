43c64daa0199835a2c9ae3476766331fb24e6f97
Ahmad <ahmadrozak@versatech.co.id>
2022-12-12 18:06:26 +0700

    mantap
    Signed-off-by: Ahmad <ahmadrozak@versatech.co.id>

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
 
 <style lang="scss">
     /*Import Main styles for this application*/
+    /*@import "styles/bootstrap";*/
+    /*@import "styles/app";*/
+    /*@import "styles/custom.css";*/
     @import "styles/bootstrap";
     @import "styles/app";
     @import "styles/custom.css";
